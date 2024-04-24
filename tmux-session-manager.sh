e31e0765aa963d9a76fb0bd0652c3daf690d8b57
Mehdi Safaee <mehdi0xc@gmail.com>
2024-04-23 19:13:53 -0700

    Add code

---
+++ tmux-session-manager.sh
@@ -0,0 +1,34 @@
+# By mehdi0xc, 202404
+function muxm() {
+
+local sessions
+sessions=$(tmux ls -F "#{session_name}" 2>/dev/null) || true
+sessions="[new session]
+[kill sessions]
+${sessions}"
+
+export FZF_DEFAULT_OPTS='--color=fg:#ffffff,hl:#20A0F0,fg+:#ffffff,hl+:#20A0F0,info:#20A0F0,prompt:#ffffff,pointer:#20A0F0,marker:#20A0F0'
+
+local session
+session=$(echo "$sessions" | fzf --height 40% --reverse)
+
+if [[ "$session" == '[new session]' ]]; then
+local new_session
+vared -p 'Enter new session name: ' -c new_session
+tmux new-session -d -s "$new_session" 'zsh'
+tmux attach-session -t "$new_session"
+elif [[ "$session" == '[kill sessions]' ]]; then
+local kill_sessions
+kill_sessions=$(tmux ls -F "#{session_name}" 2>/dev/null | fzf --height 40% --multi --reverse)
+if [[ -n "$kill_sessions" ]]; then
+    echo "$kill_sessions" | while read -r kill_session; do
+    tmux kill-session -t "$kill_session"
+    done
+fi
+else
+if [ -n "$session" ]; then
+    tmux attach-session -t "$session"
+fi
+fi
+
+}
